cfdcecb186b7966c7da96db844b2d0c5b32bb0ec
Mohit Namdev <namdevmohit0@gmail.com>
2023-02-11 01:53:17 +0530

    checkpoint 1

--- Backend/registration.js
+++ Backend/registration.js
@@ -1,23 +1,52 @@
-const users = [];
-
-function signUp(email, password) {
-    const user = {
-      email: email,
-      password: password
-    };
-    users.push(user);
-    console.log("User signed up successfully");
+// Create an object to store users' information
+let users = [
+    {
+      email: "user1@example.com",
+      password: "pass123"
+    },
+    {
+      email: "user2@example.com",
+      password: "pass456"
+    },
+    {
+        email:"Mohit",
+        password:"mohit2003"
+    }
+  ];
+  
+  function signUp(email, password) {
+    // existing user
+    let existingUser = users.find(user => user.email === email);
+    if (existingUser) {
+      return "Email is already in use";
+    } else {
+      // Add the new user to the users array
+      let newUser = {
+        email: email,
+        password: password
+      };
+      users.push(newUser);
+      return "Sign up successful";
+    }
   }
-
+  
+  // Function to sign in
   function signIn(email, password) {
-    for (const user of users) {
-      if (user.email === email && user.password === password) {
-        console.log("User signed in successfully");
-        return;
+    let existingUser = users.find(user => user.email === email);
+    if (!existingUser) {
+      return "Email not found";
+    } else {
+        
+      if (existingUser.password === password) {
+        return "Sign in successful";
+      } else {
+        alert("Incorrect password");
       }
     }
-    console.log("Sign in failed: Invalid email or password");
   }
-
-signUp("john.doe@example.com", "password123");
-signIn("john.doe@example.com", "password23");
+  
+  // Example usage
+  console.log(signUp("user3@example.com", "pass789"));
+  console.log(signIn("user3@example.com", "pass789"));
+  console.log(signIn("Mohit", "mohit2002"));
+  
