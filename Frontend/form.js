e29d5d0515607d2c9fdd9a3020d6bd35034bb424
Mohit Namdev <namdevmohit0@gmail.com>
2023-02-11 22:34:08 +0530

    added FAQ

---
+++ Frontend/form.js
@@ -0,0 +1,8 @@
+
+  document.getElementById("userForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const userData = { name, email };
+    console.log(JSON.stringify(userData));
+  });
