83b0f0212181ef0c4241ddacd89a9e8b447d1e2b
Mohit Namdev <namdevmohit0@gmail.com>
2023-02-12 03:57:13 +0530

    added FAQ section

--- Frontend/faq.js
+++
@@ -1,7 +0,0 @@
-const accordionHeaders = document.querySelectorAll('.accordion-header')
-
-accordionHeaders.forEach(header => {
-  header.addEventListener('click', () => {
-    header.parentElement.classList.toggle('active')
-  })
-})
