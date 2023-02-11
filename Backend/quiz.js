71df4b7a0c0219ae4f6c34b873f0c82e86b2f1a4
Mohit Namdev <namdevmohit0@gmail.com>
2023-02-11 10:54:51 +0530

    added quiz functionality

--- Backend/quiz.js
+++ Backend/quiz.js
@@ -1,92 +1,59 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Quiz questions and answers
-const questions = [
-    {
-      question: "What is JavaScript?",
-      answers: [
-        { text: "A programming language", correct: true },
-        { text: "A type of coffee", correct: false },
-        { text: "A browser", correct: false },
-        { text: "An operating system", correct: false }
-      ]
-    },
-    {
-      question: "What is the syntax for declaring a variable in JavaScript?",
-      answers: [
-        { text: "var name = value", correct: true },
-        { text: "variable name = value", correct: false },
-        { text: "const name = value", correct: false },
-        { text: "let name = value", correct: false }
-      ]
-    },
-    {
-      question: "What is the syntax for an if statement in JavaScript?",
-      answers: [
-        { text: "if (condition) { ... }", correct: true },
-        { text: "if condition { ... }", correct: false },
-        { text: "if: condition { ... }", correct: false },
-        { text: "if condition: { ... }", correct: false }
-      ]
-    }
-  ];
-  
-  // Keep track of the user's score
-  let score = 0;
-  
-  // Keep track of the current question
-  let currentQuestion = 0;
-  
-  // Get references to the quiz container and buttons
-  const quizContainer = document.getElementById("quiz");
-  const submitButton = document.getElementById("submit");
-  
-  // Show the first question
-  showQuestion();
-  
-  // Event listener for the submit button
-//   submitButton.addEventListener("click", showResults);
-  
-  // Display the current question and answers
-  function showQuestion() {
-    const question = questions[currentQuestion].question;
-    const answers = questions[currentQuestion].answers;
-  
-    quizContainer.innerHTML = `
-      <h2>${question}</h2>
-      ${answers
-        .map(
-          (answer, index) => `
-          <div class="form-check">
-            <input type="radio" name="answer" id="answer${index}" value="${index}">
-            <label for="answer${index}">${answer.text}</label>
-          </div>
-        `
-        )
-        .join("")}
-    `;
+const quizData = [
+  {
+    question: "What is the capital of France?",
+    options: [
+      { text: "Paris", isCorrect: true },
+      { text: "London", isCorrect: false },
+      { text: "Berlin", isCorrect: false },
+      { text: "Rome", isCorrect: false }
+    ]
+  },
+  {
+    question: "What is the currency used in Japan?",
+    options: [
+      { text: "Yen", isCorrect: true },
+      { text: "Euro", isCorrect: false },
+      { text: "Dollar", isCorrect: false },
+      { text: "Pound", isCorrect: false }
+    ]
   }
-  
-  // Show the results of the quiz
-  function showResults() {
-    // Get the selected answer
-    const selected = document.querySelector("input[name=answer]:checked");
-    const answer = questions[currentQuestion].answers[selected.value];
-  
-    // Update the score if the answer is correct
-    if (answer.correct) {
+];
+
+const form = document.getElementById("quiz-form");
+const submitBtn = document.getElementById("submit-btn");
+const resultDiv = document.getElementById("result");
+
+quizData.forEach((quizQuestion, index) => {
+  form.innerHTML += `
+    <div class="question">
+      <h2>Question ${index + 1}: ${quizQuestion.question}</h2>
+      <div class="options">
+        ${quizQuestion.options
+          .map(
+            (option, optionIndex) => `
+              <label>
+                <input type="radio" name="question-${index}" value="${optionIndex}" required>
+                ${option.text}
+              </label>
+            `
+          )
+          .join("")}
+      </div>
+    </div>
+  `;
+});
+
+submitBtn.addEventListener("click", e => {
+  e.preventDefault();
+  let score = 0;
+  quizData.forEach((quizQuestion, index) => {
+    const selectedOption = form[`question-${index}`].value;
+    if (quizQuestion.options[selectedOption].isCorrect) {
       score++;
     }
-  
-    // Move on to the next question, or show the results if there are no more questions
-    currentQuestion++;
-    if (currentQuestion === questions.length) {
-      quizContainer.innerHTML = `
-        <h2>You got ${score} out of ${questions.length} questions correct!</h2>
-      `;
-    } else {
-      showQuestion();
-    }
-  }
-  
+  });
+  resultDiv.innerHTML = `
+    <h2>Result</h2>
+    <p>You scored ${score}/${quizData.length}</p>
+  `;
 });
-  
