5b71705f3d9829c85e33c8e271c842cec0cd7252
Mohit Namdev <namdevmohit0@gmail.com>
2023-02-12 09:18:31 +0530

    added more employers in JSON

--- Backend/data.js
+++ Backend/data.js
@@ -13,24 +13,43 @@ const employeerData =
   "vacancies" : 10,
   "Sector" : "Cloud",
   "Date" : "2022-02-01",
-  "desc" : "TCS is Hiring Dev-Ops Engineers for batch "
+  "desc" : "TCS is Hiring Dev-Ops Engineers for batch 2023"
 },
 {
-  "Name" : "abc",
-  "Skills" : "Mern Stack Developer",
-  "vacancies" : 5,
-  "Sector" : "xyz",
-  "Date" : "2022-02-12",
-  "desc" : "We need a MERN Stack Developer"
+  "Name" : "Accenture",
+  "Skills" : "Java Enterprise Developer",
+  "vacancies" : 4,
+  "Sector" : "JAVA",
+  "Date" : "2022-02-10",
+  "desc" : "Accenture is Hiring Java Enterprise Developer"
 },
 {
-  "Name" : "abc",
-  "Skills" : "Mern Stack Developer",
+  "Name" : "Wipro",
+  "Skills" : "Cyber Security Analyst",
   "vacancies" : 5,
-  "Sector" : "xyz",
-  "Date" : "2022-02-12",
-  "desc" : "We need a MERN Stack Developer"
+  "Sector" : "Cyber Computing",
+  "Date" : "2022-02-05",
+  "desc" : "Wipro is hiring Cyber Security Analyst"
 },
+
+{
+  "Name" : "Cognizant",
+  "Skills" : "Pega Development Professional",
+  "vacancies" : 5,
+  "Sector" : "Engineering",
+  "Date" : "2022-02-01",
+  "desc" : "Pega Development Professional. 2- 9 of experience in Pega Development with Pega relevant certification. Good knowledge around integration and exposure to REST and SOAP APIs."
+},
+
+{
+  "Name" : "IBM",
+  "Skills" : "Infrastructure Specialist",
+  "vacancies" : 10,
+  "Sector" : "Cyber Application Operation",
+  "Date" : "2022-02-04",
+  "desc" : "IBM hiring Technical Infrastructure Specialist "
+},
+
     ];
 
 
