64fcee375c482b03a0114c324ec4bac5ec9b9669
Mohit Namdev <namdevmohit0@gmail.com>
2023-02-12 08:57:53 +0530

    Added JSON data for employeer and UI changes

---
+++ Backend/data.js
@@ -0,0 +1,39 @@
+const employeerData = 
+    [{
+        "Name" : "Infosys",
+        "Skills" : "Mern Stack Developer",
+        "vacancies" : 5,
+        "Sector" : "Development",
+        "Date" : "2022-02-12",
+        "desc" : "We need a MERN Stack Developer"
+},
+{
+  "Name" : "TCS",
+  "Skills" : "Dev-Ops Engineer",
+  "vacancies" : 10,
+  "Sector" : "Cloud",
+  "Date" : "2022-02-01",
+  "desc" : "TCS is Hiring Dev-Ops Engineers for batch "
+},
+{
+  "Name" : "abc",
+  "Skills" : "Mern Stack Developer",
+  "vacancies" : 5,
+  "Sector" : "xyz",
+  "Date" : "2022-02-12",
+  "desc" : "We need a MERN Stack Developer"
+},
+{
+  "Name" : "abc",
+  "Skills" : "Mern Stack Developer",
+  "vacancies" : 5,
+  "Sector" : "xyz",
+  "Date" : "2022-02-12",
+  "desc" : "We need a MERN Stack Developer"
+},
+    ];
+
+
+console.log("The Current employeer is ",employeerData[0].Name);
+
+console.log("The Current employeer is ",employeerData[0].Skills);
